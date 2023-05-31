27653fff424f270e832e3b31d0e627cf911c6fc1
Kumar Neeraj <33743260+KNeeraj038@users.noreply.github.com>
2023-05-31 18:30:48 +0530

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -3,9 +3,9 @@
 
 nrj_connect_to_vpn(){
     # Set the required inputs
-    local username="kneeraj"
-    local password="Nrj9801223938NN"
-    local domain="LocalDomain"
+    local username="<user_name>"
+    local password="<pwd>"
+    local domain="<domain_name>"
     local server="14.97.141.34:4433"
 
     # Run the NetExtender executable with the provided inputs
